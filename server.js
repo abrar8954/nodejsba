8e273f6d53b6a36f4f4867d3eaa3b8555f48de0b
Abrar Sardar <abrarsardar8955@gmail.com>
2023-07-20 15:10:02 +0500

    Changed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const admin =  require('firebase-admin')
+const admin = require('firebase-admin')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 var serviceAccount = require("./pushnotificationbtwdevices-firebase-adminsdk-ei1wh-6fca19de6a.json");
 app.use(express.json())
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 //    const message = {
@@ -30,28 +30,38 @@ admin.initializeApp({
 // }) 
 
 
-app.post('/send-noti',(req,res)=>{
+app.post('/send-noti', (req, res) => {
     console.log('req.body.tokens: ', req.body.tokens)
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     // res.end('HI there');
-   const message = { 
-    notification:{
-        title:"Hi! Abrar",
-        body:"Where are you going? man.",
-        image: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e',
-    },
-    tokens:req.body.tokens
-    
-}
+    const message = {
+        notification: {
+            title: "Hi! Abrar",
+            body: "Where are you going? man.",
+            image: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e',
+        },
+        android: {
+            notification: {
+                imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e'
+            }
+        },
+        webpush: {
+            headers: {
+                image: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e'
+            }
+        },
+        tokens: req.body.tokens
 
-admin.messaging().sendMulticast(message).then(res=>{
-   console.log('send success')
-}).catch(err=>{
-    console.log(err)
-}) 
+    }
+
+    admin.messaging().sendMulticast(message).then(res => {
+        console.log('send success')
+    }).catch(err => {
+        console.log(err)
+    })
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('server running')
 })

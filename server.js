be47508d04e69c0da44e5d0dbd6ef95206c8c43d
Abrar Sardar <abrarsardar8955@gmail.com>
2023-07-20 14:04:42 +0500

    Changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-var serviceAccount = require("./pushnotificationbtwdevices-firebase-adminsdk-ei1wh-fcc260a69a.json");
+var serviceAccount = require("./pushnotificationbtwdevices-firebase-adminsdk-ei1wh-6fca19de6a.json");
 app.use(express.json())
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -31,14 +31,15 @@ admin.initializeApp({
 
 
 app.post('/send-noti',(req,res)=>{
-    console.log(req.body)
+    console.log('req.body.tokens: ', req.body.tokens)
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     // res.end('HI there');
-   const message = {
+   const message = { 
     notification:{
-        title:"new ad",
-        body:"new ad posted click to open"
+        title:"title",
+        body:"new ad posted click to open",
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e',
     },
     tokens:req.body.tokens
     

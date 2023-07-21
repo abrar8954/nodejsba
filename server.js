252b15b5bb734b8b8b4352c34f3a316404d4a9ad
Abrar Sardar <abrarsardar8955@gmail.com>
2023-07-21 14:26:30 +0500

    Changed

--- server.js
+++ server.js
@@ -30,17 +30,43 @@ admin.initializeApp({
 // }) 
 
 
+async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+        title: 'Notification Title',
+        body: 'Main body content of the notification',
+        android: {
+            channelId,
+            smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+            // pressAction is needed if you want the notification to open the app when pressed
+            pressAction: {
+                id: 'default',
+            },
+        },
+    });
+}
+
 app.post('/send-noti', (req, res) => {
     console.log('req.body.tokens: ', req.body.tokens)
     // res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/plain');
     // res.end('HI there');
     const message = {
-        notification: {
-            title: "Hi! Abrar",
-            body: "Where are you going? man.",
-            
-        },
+        // notification: {
+        //     title: "Hi! Abrar",
+        //     body: "Where are you going? man.",
+
+        // },
+        notification: onDisplayNotification(),
         tokens: req.body.tokens
 
     }

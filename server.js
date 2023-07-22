3e1aa80a8881b29c5efa7797984997b20a0e6d32
Abrar Sardar <abrarsardar8955@gmail.com>
2023-07-22 16:00:27 +0500

    Changed

--- server.js
+++ server.js
@@ -71,71 +71,57 @@ app.post('/send-noti', (req, res) => {
     //     // notification: onDisplayNotification(),
     //     tokens: req.body.tokens
     // }
-    
 
-    const message = {
-        data: {
-            notifee: JSON.stringify(
-                
-            //     {
-            //     title: 'Hi there',
-            //     body: 'This message was sent via FCM!',
-            //     android: {
-            //         channelId: 'default',
-            //         largeIcon: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e',
-            //         actions: [
-            //             {
-            //                 title: 'Mark as Read',
-            //                 pressAction: {
-            //                     id: 'read',
-            //                 },
-            //             },
-            //         ],
-            //     },
-            // }
-            
-
-            {
-                title: 'Sarah Lane',
-                body: 'Great thanks, food later?',
-                android: {
-                  channelId,
-                  style: {
-                    type: AndroidStyle.MESSAGING,
-                    person: {
-                      name: 'John Doe',
-                      icon: 'https://my-cdn.com/avatars/123.png',
-                    },
-                    messages: [
-                      {
-                        text: 'Hey, how are you?',
-                        timestamp: Date.now() - 600000, // 10 minutes ago
-                      },
-                      {
-                        text: 'Great thanks, food later?',
-                        timestamp: Date.now(), // Now
-                        person: {
-                          name: 'Sarah Lane',
-                          icon: 'https://my-cdn.com/avatars/567.png',
+        const message = {
+            data: {
+                notifee: JSON.stringify({
+                    title: 'Hi there',
+                    body: 'This message was sent via FCM!',
+                    android: {
+                        channelId: 'default',
+                        largeIcon: 'https://firebasestorage.googleapis.com/v0/b/dukanfiba.appspot.com/o/images%2FshopImages%2F-NWDYgcOggK8JILReyRt?alt=media&token=58ede0d3-7b8a-4e51-8228-fd35660bc49e',
+                        actions: [
+                            {
+                                title: 'Mark as Read',
+                                pressAction: {
+                                    id: 'read',
+                                },
+                            },
+                        ],
+                        style: {
+                            type: AndroidStyle.MESSAGING,
+                            person: {
+                                name: 'John Doe',
+                                icon: 'https://my-cdn.com/avatars/123.png',
+                            },
+                            messages: [
+                                {
+                                    text: 'Hey, how are you?',
+                                    timestamp: Date.now() - 600000, // 10 minutes ago
+                                },
+                                {
+                                    text: 'Great thanks, food later?',
+                                    timestamp: Date.now(), // Now
+                                    person: {
+                                        name: 'Sarah Lane',
+                                        icon: 'https://my-cdn.com/avatars/567.png',
+                                    },
+                                },
+                            ],
                         },
-                      },
-                    ],
-                  },
-                },
-            }
+                    },
+                }),
+            },
+            tokens: req.body.tokens
 
-            ),
-        },
-        tokens: req.body.tokens
+        }
 
-    }
-
-    admin.messaging().sendMulticast(message).then(res => {
-        console.log('send success')
-    }).catch(err => {
-        console.log(err)
+        admin.messaging().sendMulticast(message).then(res => {
+            console.log('send success')
+        }).catch(err => {
+            console.log(err)
+        })
     })
-})
 
 app.listen(port, () => {
     console.log('server running')
